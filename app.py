c5a7f488e70eea7008a3fa690ea03895ed7667a3
Aurmoun <25367158+Aurmoun@users.noreply.github.com>
2018-01-18 14:05:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def makeYqlQuery(req):
     if city is None:
         return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "'and u='c')"
 
 
 def makeWebhookResult(data):
