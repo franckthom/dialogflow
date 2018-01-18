8be74bc2bea5a4836d8edbaf3f37500c6f973e30
Aurmoun <25367158+Aurmoun@users.noreply.github.com>
2018-01-18 12:31:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -60,28 +60,9 @@ def processRequest(req):
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
-    elif req.get("result").get("action")=="getjoke":
-        baseurl = "http://api.icndb.com/jokes/random"
-        result = urlopen(baseurl).read()
-        data = json.loads(result)
-        res = makeWebhookResultForGetJoke(data)
-    else:
-        return {}
     
     return res
 
-def makeWebhookResultForGetJoke(data):
-    valueString = data.get('value')
-    joke = valueString.get('joke')
-    speechText = joke
-    displayText = joke
-    return {
-        "speech": speechText,
-        "displayText": displayText,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
 
 
 def makeYqlQuery(req):
