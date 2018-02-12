8dd1df160e94106a4c4d0bf39e6ec59bbca3c0d4
Aurmoun <25367158+Aurmoun@users.noreply.github.com>
2018-02-12 14:19:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def makeGsSesQuery(req):
 
 #fonction afin d'afficher API googlesheet pour session
 def makeWebhookResultForSheetsSes(data):
-    for index in range(data):
+    for index in range(len(data)):
        nom1 = data[index]['nom session']
        #nom2 = data[index-1]['nom session']
     #date = data[0]['date']
