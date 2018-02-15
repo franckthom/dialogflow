0ae86b7bf43a3adafae7714d741f781fc95ac7de
Aurmoun <25367158+Aurmoun@users.noreply.github.com>
2018-02-15 15:59:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ def makeWebhookResultForSheetsSes(data):
     data_len = len(data)
     for i in range(0, data_len):
         nom1 = data[i]['nom session']
-    #date = data[0]['date']
+        nom2 = nom3.append(nom1)
         speech = "la session " + nom1
     return {
        "speech": speech,
