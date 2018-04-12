f6e5c277c047f0957574a1d4d2f9a30696f24424
Aurmoun <a.mounier@ecole-ipssi.net>
2018-04-12 14:28:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def processRequest(req):
     elif req.get("result").get("action")=="readsheet-exp":
         GsExp_query = makeGsExpQuery(req)
         ChatBasequery(req)
-        chatbaseProcess()
+        chatbaseProcess(message_user, metadata, intent, timestamp, user_id, platform, api_key)
         client = SheetsuClient("https://sheetsu.com/apis/v1.0su/27ac2cb1ff16")
         data = client.search(sheet="Exposant", nom=GsExp_query)
         res = makeWebhookResultForSheetsExp(data)
