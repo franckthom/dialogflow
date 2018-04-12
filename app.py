f6e0b9b15504fdfa138f8a546157bddf36aa77a7
Aurmoun <a.mounier@ecole-ipssi.net>
2018-04-12 16:20:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -104,6 +104,15 @@ def processRequest(req):
         client = SheetsuClient("https://sheetsu.com/apis/v1.0su/27ac2cb1ff16")
         data = client.read(sheet="Conference") 
         res = makeWebhookResultForSheetsSesNow(data)
+    elif ChatBasequery(req):
+        result = req.get("result")
+        message_user = result.get("resolvedQuery")
+        metadata = result.get("metadata")
+        intent = metadata.get("intentName")
+        timestamp = req.get("timestamp")
+        user_id = req.get("id")
+        platform = 'Dialogflow'
+        api_key = '56bd0b2b-4b67-4522-8933-1ff443a8a922'
     
     else:
         return {}
@@ -112,48 +121,12 @@ def processRequest(req):
 
 #def chatbaseProcess(message_user, metadata, intent, timestamp, user_id, platform, api_key):
     
-   
+   #return None
     
-    #return None
     
-
-def ChatBasequery(req):
-    result = req.get("result")
-    message_user = result.get("resolvedQuery")
-    metadata = result.get("metadata")
-    intent = metadata.get("intentName")
-    timestamp = req.get("timestamp")
-    user_id = req.get("id")
-    platform = 'Dialogflow'
-    api_key = '56bd0b2b-4b67-4522-8933-1ff443a8a922'
+     
     
-     # Create an instance of MessageSet to collect all the messages
-    message_set = MessageSet(api_key=api_key, platform=platform,
-             version=version, user_id=user_id)
-    # Create an instance of Message for the user message and set values in the constructor
-    msg1 = Message(api_key=api_key, platform=platform, message=message,
-            intent=intent, version=version, user_id=user_id,
-            type=MessageTypes.USER, not_handled=True,
-            time_stamp=time_stamp)
-    # Set the message as "handled" because the NLP was able to successfully decode the intent
-    msg1.set_as_feedback()
-
-    # Create an instance of Message for the bot response message and set values in the constructor
-    msg2 = Message(api_key=api_key, platform=platform, message=message,
-            version=version, user_id=user_id,
-            type=MessageTypes.AGENT)
-    message_set = MessageSet(api_key=api_key, platform=platform,
-                         version=version, user_id=user_id)
-
-    # Push messages into the collection (MessageSet)
-    message_set.append_message(msg1)
-    message_set.append_message(msg2)
-
-    # Send the messages
-    response = message_set.send()
-    # response.status_code will be 200 if sending worked
     
-    return message_user, metadata, intent, timestamp, user_id, platform, api_key
     
     
 #fonction pour créer la query pour exposant
