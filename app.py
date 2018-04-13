ecb23baf4d67cf6afedbfc7964dfe4a69663e59a
Aurmoun <a.mounier@ecole-ipssi.net>
2018-04-13 14:27:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
+    chatbaseProcess(req)
     print("Request:")
     print(json.dumps(req, indent=4))
 
