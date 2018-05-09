ecdff6851dccb28a58d81506600bc6f4aa5faf22
Aurmoun <a.mounier@ecole-ipssi.net>
2018-05-09 17:48:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -138,7 +138,8 @@ def processChatbase(req):
   set = MessageSet(api_key = '56bd0b2b-4b67-4522-8933-1ff443a8a922',
                    platform = 'Dialogflow',
                    version = "0.1",
-                   user_id = req.get("id"))
+                   user_id = req.get("id"),
+                   not_handled=True)
   msg = set.new_message(intent = metadata.get("intentName"),message = result.get("resolvedQuery"))
 
   msg2 = Message(api_key='56bd0b2b-4b67-4522-8933-1ff443a8a922',
