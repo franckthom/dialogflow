07746cbee808455d1d5dba27ab72588f5296c3a6
Aurmoun <a.mounier@ecole-ipssi.net>
2018-05-31 17:41:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def webhook():
     
     res = processRequest(req)
     
-    res = json.dumps(res, indent=4)qs
+    res = json.dumps(res, indent=4)
     # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
